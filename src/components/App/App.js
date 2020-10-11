e94e70772dcd85929427f68a8ada6935625affa7
Ahmed <azzoghayyer@gmail.com>
2020-10-11 12:19:43 -0700

    Add optional chaining

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 
 const AppContainer = (props) => (
-  <Provider store={props.store}>
+  <Provider store={props?.store}>
     <div>
       <h2>Waseel Web App</h2>
     </div>
